9f12cb50697193126d55d3c2d43a573a1d1e57cd
Bartolomeo Müller <bartolomeo.mueller@stud.h-da.de>
2021-09-26 13:46:12 +0200

    niiiice

---
+++ src/components/argument.svelte
@@ -0,0 +1,20 @@
+<div class="mt-4">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class="h-10 w-10 stroke-current text-green-500 m-auto"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      stroke-width="2"
+      d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+    />
+  </svg>
+
+  <div class="p-4 m-4 border-2 border-green-200 bg-green-100 rounded-lg">
+    <p>If the dummy looks nice, everything should look nice!</p>
+  </div>
+</div>
--- src/routes/theses/dummy.svelte
+++ src/routes/theses/dummy.svelte
@@ -1,15 +1,14 @@
+<script>
+  import Argument from '../../components/argument.svelte';
+</script>
+
 <div class="py-12">
   <div class="text-center">
     <h2 class="text-xl text-indigo-600">Dummy</h2>
     <h1 class="text-3xl mt-2 font-semibold">Should the Dummy look nice?</h1>
   </div>
 
-  <div class="flex bg-gray-200">
-    <div class="p-4 m-8 bg-red-500 border-2 border-green-700">
-      If the dummy looks nice, everything should look nice!
-    </div>
-    <div class="p-4 m-8 bg-blue-500 border-2 border-pink-900">
-      But the dummy is not that important!
-    </div>
+  <div class="flex bg-gray-100 rounded-3xl">
+    <Argument />
   </div>
 </div>
